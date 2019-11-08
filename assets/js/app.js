ef8cd568b9091af7cf12e90d7f6726ace173fcc5
Sepehr Hosseini <iamsepehrhosseini@gmail.com>
2019-11-08 17:50:32 +0330

    feat(discount): add tab

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('.discount-tabs').slick({
+    slidesToShow: 9,
+    slidesToScroll: 1,
+    rtl: true,
+    prevArrow: '<button type="button" class="slick-prev"><</button>',
+    nextArrow: '<button type="button" class="slick-next">></button>',
+  })
+})
